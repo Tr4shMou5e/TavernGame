cc9e9b4fe2f8389cc95a5807f5f9fcf4c4cc60dc
charlie <purvischa@gmail.com>
2026-02-09 01:49:52 -0500

    Title and main menu screens added

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void NewGame()
+    {
+        Debug.Log("Starting New Game...");
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Load Game clicked (not implemented yet)");
+        // Later: Load save file, then SceneManager.LoadScene("MainGame");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Settings opened (placeholder)");
+        // Later: Enable settings panel
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting game...");
+        Application.Quit();
+    }
+}
