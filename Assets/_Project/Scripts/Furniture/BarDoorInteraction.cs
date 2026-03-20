b954a89e50fd2e6d05aa5596324e6f2fef377168
Ryuality <139538767+Ryuality@users.noreply.github.com>
2026-03-19 20:23:11 -0400

    Added door and bar door animations

---
+++ Assets/_Project/Scripts/Furniture/BarDoorInteraction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BarDoorInteraction : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private float crossFadeDuration = 0.5f;
+    private readonly int doorOpenHash = Animator.StringToHash("Bar_Door_Open");
+    private readonly int doorCloseHash = Animator.StringToHash("Bar_Door_Close");
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        animator.CrossFade(doorOpenHash, crossFadeDuration);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        animator.CrossFade(doorCloseHash, crossFadeDuration); 
+    }
+}
