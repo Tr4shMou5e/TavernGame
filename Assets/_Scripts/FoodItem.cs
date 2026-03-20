eed298cc4e57ceb3bd0698b39bd5780782b95eb5
charlie <purvischa@gmail.com>
2026-03-19 21:25:51 -0400

    Revert "Merge branch 'main' into charlie"
    This reverts commit 3ff439cd9f90cb54114e8033e19cd44bc80e9a23, reversing
    changes made to a59f2d297ccd2d7bebe51782b93edb95bab0b7ef.

--- Assets/_Scripts/FoodItem.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "FoodItems", menuName = "Scriptable Objects/FoodItems")]
-public class FoodItems : ScriptableObject
-{
-    public string itemName;
-    public Sprite icon;
-}
