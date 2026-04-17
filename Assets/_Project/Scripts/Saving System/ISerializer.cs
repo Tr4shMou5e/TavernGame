bb7b7894b4270e3537e732e59951c778e67dbb6a
Rund Abu <105173648+Tr4shMou5e@users.noreply.github.com>
2026-04-17 14:49:07 -0400

    Revert "Merge branch 'Dialogue-System' into MainWorld"
    This reverts commit fa5a290a14bf71012f084086cd4ad84ca3b9f39e, reversing
    changes made to ae624db5f6e94f957b3a417eef0eb1d3143eeb8f.

--- Assets/_Project/Scripts/Saving System/ISerializer.cs
+++
@@ -1,5 +0,0 @@
-public interface ISerializer
-{
-    string Serialize<T>(T obj);
-    T Deserialize<T>(string json);
-}
--- Assets/_Project/Scripts/State Machine/ITransition.cs
+++
@@ -1,5 +0,0 @@
-﻿public interface ITransition
-{
-    IState To { get; }
-    IPredicate Condition { get; }
-}
