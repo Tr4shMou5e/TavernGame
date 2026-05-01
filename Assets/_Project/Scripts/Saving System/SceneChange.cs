de91776ab3715329978f30feb68df142e5c17789
Ryuality <139538767+Ryuality@users.noreply.github.com>
2026-05-01 19:30:26 -0400

    Update

---
+++ Assets/_Project/Scripts/Saving System/SceneChange.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    [SerializeField] SceneNameType sceneName;
+     
+    private bool isTriggered;
+    
+    public static event Action<string> OnSceneChange;
+    private void Update()
+    {
+        if (!isTriggered) return;
+        if(InputManager.Instance.Interact())
+        {
+            OnSceneChange?.Invoke(sceneName.ToString());
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+            return;
+        isTriggered = true;
+        
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+            return;
+        isTriggered = false;
+    }
+
+    private enum SceneNameType
+    {
+        MainWorld,
+        Tavern,
+        Home
+    }
+}
