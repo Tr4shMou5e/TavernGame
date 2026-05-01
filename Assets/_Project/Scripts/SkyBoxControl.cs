d0f96868806f50b85710994e2a41fd5309e52384
Rund Abu <105173648+Tr4shMou5e@users.noreply.github.com>
2026-05-01 01:53:02 -0400

    extraStuff

---
+++ Assets/_Project/Scripts/SkyBoxControl.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SkyBoxControl : MonoBehaviour
+{
+    [SeralizeField] Renderer skyBox;
+    private Material mat;
+
+    private void Awake()
+    {
+        mat = RenderSettings.skyBox;
+    }
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
