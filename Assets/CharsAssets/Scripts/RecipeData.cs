1a29f00728b5885b97b63792a0bc780b458fe1b9
Ryuality <139538767+Ryuality@users.noreply.github.com>
2026-05-02 05:16:30 -0400

    save

--- Assets/CharsAssets/Scripts/RecipeData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Tavern/Recipe")]
-public class RecipeData : ScriptableObject
-{
-    public string recipeName;
-    public string[] ingredients;
-    public bool isDrink;
-}
