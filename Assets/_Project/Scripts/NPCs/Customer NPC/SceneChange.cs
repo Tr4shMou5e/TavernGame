841c5875818834736b8aca01b8fde69742a18757
Ryuality <139538767+Ryuality@users.noreply.github.com>
2026-05-01 21:17:12 -0400

    Added Scene Transition Scripts

---
+++ Assets/_Project/Scripts/NPCs/Customer NPC/SceneChange.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    [SerializeField] SceneNameType sceneName;
+    
+    private bool isTriggered;
+    
+    public static event Action<string> OnSceneChange;
+    void Update()
+    {
+        if(!isTriggered) return;
+        if(InputManager.Instance.Interact())
+        {
+            OnSceneChange?.Invoke(sceneName.ToString());
+        }
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+            return;
+        isTriggered = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(!isTriggered)
+            return;
+        isTriggered = false;
+    }
+
+    private enum SceneNameType
+    {
+        MainMenu,
+        Tavern,
+        Home
+    }
+}
